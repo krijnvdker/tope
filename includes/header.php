aa5300a0a66a366c42b1da983ce233c9ed816791
Krijn vd Kerkhof <krijn@exitable.nl>
2018-12-21 10:58:50 +0100

    added navigation

---
+++ includes/header.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+	
+<!--[if lt IE 7]> <html class="no-js lt-ie10 lt-ie9 lt-ie8 lt-ie7 ie" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie10 lt-ie9 lt-ie8 ie" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie10 lt-ie9 ie" lang="en"> <![endif]-->
+<!--[if IE 9]>    <html class="no-js lt-ie10 ie" lang="en"> <![endif]-->
+<!--[if IE 10]>    <html class="no-js ie" lang="en"> <![endif]-->
+<!-- Consider adding a manifest.appcache: h5bp.com/d/Offline -->
+<!--[if gt IE 10]><!--> <html class="no-js" lang="nl"> <!--<![endif]-->
+<head>
+
+  	<meta charset="utf-8">
+  	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+  	<title>Jorrit Hendrix U11 Tournament</title>
+  	<meta name="description" content="Het Jorrit Hendrix Under-11 tournament wordt dit jaar voor het derde jaar georganiseerd op voetbalvelden van SV Panningen. Gevestigde namen doen mee zoals: Schalke 04, Wolverhampton Wanderers en nog veel meer.">
+  	<meta name="keywords" content="Jorrit Hendrix U11 Tournament"/>
+  
+
+  	<!--if mobile is supported-->
+  	<!-- end if -->
+
+	<!-- Mobile viewport optimized: h5bp.com/viewport -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	  	<!-- Place favicon.ico and apple-touch-icon.png in the root directory: mathiasbynens.be/notes/touch-icons -->
+	  	<link rel="icon" type="image/png" href="img/favicon.png" sizes="64x64">
+	  	<!-- Home screen icon -->
+	  	<!-- For iPhone 4 with high-resolution Retina display: -->
+	  	<!-- <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png"> -->
+	  	<!-- For first-generation iPad: -->
+	  	<!-- <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png"> -->
+	  	<!-- For non-Retina iPhone, iPod Touch, and Android 2.1+ devices: -->
+	  	<!-- <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png"> -->
+
+	  	<!-- Mobile IE allows us to activate ClearType technology for smoothing fonts for easy reading -->
+	  	<meta http-equiv="cleartype" content="on"/>
+
+	<!--Facebook data voor sharing-->
+	<meta property="og:title" content="Jorrit Hendrix U11 Tournament" />
+	<meta property="og:url" content="http://www.jorrithendrixu11tournament.nl"/><!--default current URL-->
+	<meta property="og:site_name" content="Jorrit Hendrix U11 Tournament"/>
+	<meta property="og:locale" content="nl_NL"/>
+	<meta property="og:description" content="button" />
+
+	<!--Add design documents-->
+  	<link rel="stylesheet" href="css/style.css">
+
+  	<!--All JavaScript at the bottom, except this Modernizr build.
+        Modernizr enables HTML5 elements & feature detects for optimal performance.
+        Create your own custom Modernizr build: www.modernizr.com/download/ -->
+  	<script src="javascript/jquery.min.js"></script>
+  	<script src="javascript/custom.js"></script>
+
+</head>
+
+<body>
+	<nav>
+		<div class="wrapper">
+			<div class="logo">
+				<img src="img/logo1.png">
+			</div>
+			<ul>
+				<li>
+					<a href="index.php">
+						Home
+					</a>
+				</li>	
+				<li>
+					<a href="nieuws.php">
+						Nieuws
+					</a>
+				</li>
+				<li>
+					<a href="toernooi-info.php">
+						Toernooi info
+					</a>
+				</li>
+				<li>
+					<a href="deelnemers.php">
+						Deelnemers
+					</a>
+				</li>
+				<li>
+					<a href="video.php">
+						Video
+					</a>
+				</li>
+				<li>
+					<a href="sponsoren.php">
+						Sponsoren
+					</a>
+				</li>
+				<li>
+					<a href="contact.php">
+						Contact
+					</a>
+				</li>
+			</ul>
+		</div>
+	</nav>
